b0af0769a9d6a643d37c153b237ebf29d5335a5a
Sumit Paul <sumit.18.paul@gmail.com>
2021-03-09 13:59:44 +0530

    iv

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 var cacheName = 'instavid-pwa';
 var filesToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js'
+  './',
+  './index.html',
+  './style.css',
+  './script.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
