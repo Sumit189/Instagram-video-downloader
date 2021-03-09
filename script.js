67ef7b32c88ac1ec1e2b14c3b2195502de2e75e7
Sumit <sumit.18.paul@gmail.com>
2021-03-09 11:54:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker.register('./pwabuilder-sw/pwabuilder-sw.js');
   });
-
+}
 $(document).ready(function(){
 $("#private_").fadeOut('fast');
 });
